771e9679dd1ed8b83e1806448e115df5d3efe528
IvanEmanuelCamperchioli <ivanemanuel2137@gmail.com>
2021-03-10 10:14:19 -0300

    components.module added in components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
