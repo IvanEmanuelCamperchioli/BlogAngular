a881c9146b84ca83f70996571cae015025ab60ba
IvanEmanuelCamperchioli <ivanemanuel2137@gmail.com>
2021-03-10 11:51:04 -0300

    routes and footer added

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
